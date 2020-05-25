3a7ca1f31512f2db71a310e4009e76eda42c4d14
Ivan Cheung <ivan.cheungckn@gmail.com>
2020-05-25 23:47:38 +0800

    merge-update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -13,18 +12,6 @@ import { checkToken, restoreLogin } from './redux/auth/thunk';
 import FacebookLogin from './component/FacebookLogin'
 import FacebookLoginCallBack from './component/FacbookLoginCallBack';
 
-=======
-import React from 'react';
-import './App.css';
-import QuestionPage from './component/QuestionPage';
-import Question from './component/Question';
-import { Route } from 'react-router-dom';
-// import { Meetings } from './component/Meetings';
-// import { MeetingRoom } from './component/MeetingRoom';
-import { MeetingLive } from './component/MeetingLive';
-import { MeetingPast } from './component/MeetingPast';
-import { Nav } from './component/Nav';
->>>>>>> 9627f370cfabb69e8666393bf62c04939abca631
 
 function App() {
   const dispatch = useDispatch()
@@ -36,7 +23,6 @@ function App() {
   }, [dispatch, accessToken])
   return (
     <div className="App">
-<<<<<<< HEAD
       <Provider store={store}>
         
       <Switch>
@@ -63,16 +49,6 @@ function App() {
           Learn React
         </a>
       </header>
-=======
-      {/* <Nav /> */}
-      <Route path="/questions/room/:id/:page" exact>
-        <QuestionPage />
-      </Route>
-      {/* <Route path="/" exact><Meetings /></Route>
-      <Route path="/meetings/:id"><MeetingRoom /></Route> */}
-      <MeetingLive />
-      <div className="MeetingPast"><MeetingPast /></div>
->>>>>>> 9627f370cfabb69e8666393bf62c04939abca631
     </div>
   );
 }
