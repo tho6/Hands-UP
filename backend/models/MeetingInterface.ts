859c5a5c548b33bfe5e24247c494386808f40f83
Tavin Ho <chingching6@gmail.com>
2020-05-26 18:20:54 +0800

    Merge

---
+++ backend/models/MeetingInterface.ts
@@ -0,0 +1,14 @@
+export interface IMeeting {
+    id: number;
+    owner_id: number;
+    name: string;
+    code: string;
+    url: string;
+    is_live: boolean;
+    created_at: Date,
+    updated_at: Date
+    date_time: Date;
+    can_moderate: boolean;
+    can_upload_file: boolean;
+    question_limit: number;
+}
