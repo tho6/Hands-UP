3a1a313badfecb22f31fe11fcd3b4aaad31516c4
Tavin Ho <chingching6@gmail.com>
2020-05-26 19:17:59 +0800

    Merge

--- backend/services/MeetingService.ts
+++ backend/services/MeetingService.ts
@@ -5,15 +5,15 @@ export class MeetingService {
     constructor(private knex: Knex) { }
 }
 
-getAllMeeting = async ():Promise<IMeeting[]> => {
-    try {
-        const result = await this.knex.raw(
-            /*SQL*/`SELECT "id", "owner_id", "name", "code", "url", "is_live", "date_time", "can_moderate", "can_upload_file", "question_limit" FROM users`
-        )
-        return result.rows;
-    } catch (error) {
-        console.log("[MeetingService] getAllMeeting error")
-        throw error;
-    }
-}
+// getAllMeeting = async ():Promise<IMeeting[]> => {
+//     try {
+//         const result = await this.knex.raw(
+//             /*SQL*/`SELECT "id", "owner_id", "name", "code", "url", "is_live", "date_time", "can_moderate", "can_upload_file", "question_limit" FROM users`
+//         )
+//         return result.rows;
+//     } catch (error) {
+//         console.log("[MeetingService] getAllMeeting error")
+//         throw error;
+//     }
+// }
 
