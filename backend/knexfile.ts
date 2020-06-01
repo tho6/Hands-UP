a4ca47720e2723bad8b75c53ee7759ad8cf56f9a
Tavin Ho <chingching6@gmail.com>
2020-06-01 22:05:25 +0800

    Try Docker connect to PSQL

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -20,9 +20,12 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: process.env.DB_NAME,
-      user:     process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
+      database: "handsup",
+      user:     "ubuntu",
+      password: "ubuntu",
+      // database: process.env.DB_NAME,
+      // user:     process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD
     },
     pool: {
       min: 2,
