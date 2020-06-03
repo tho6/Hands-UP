68e89f85f7ca4a5a93ecf88bb7c296cf645f11c5
Rs Lam <abc@gmail>
2020-06-03 19:41:52 +0800

    remove all

--- frontend/src/component/Report.tsx
+++ frontend/src/component/Report.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchReportQuestions, fetchReportViews } from '../redux/report/thunk';
-import { ReportNavbar } from './ReportNavbar';
+// import { ReportNavbar } from './ReportNavbar';
 import { Container } from 'react-bootstrap';
-import { Backdrop } from '@material-ui/core';
+// import { Backdrop } from '@material-ui/core';
 import { useRouteMatch } from 'react-router-dom';
 import { RootState } from '../store';
-import {ViewsChart} from './ViewsChart';
+// import {ViewsChart} from './ViewsChart';
 import './Report.scss'
-import { QuestionFromChart } from './QuestionFromChart';
-import { QuestionsPieChart } from './QuestionsPieChart';
-import { QuestionLikesRank } from './QuestionLikesRank';
+// import { QuestionFromChart } from './QuestionFromChart';
+// import { QuestionsPieChart } from './QuestionsPieChart';
+// import { QuestionLikesRank } from './QuestionLikesRank';
 
 // created_at: "2020-06-20T02:00:00.000Z"
 // facebook: 3
@@ -46,12 +46,13 @@ export function Report() {
         <Container className='report-container'>
             {/* <Col md={1}> */}
             {/* <Toggler /> */}
-            <Backdrop open={isDrawerOpen}/>
+            {/* <Backdrop open={isDrawerOpen}/>
             <ReportNavbar open = {isDrawerOpen} setDrawerOpen={setDrawerOpen}/>
             <ViewsChart data={views}/>
             <QuestionFromChart data={questions}/>
             <QuestionsPieChart data={questions}/>
-            <QuestionLikesRank data={questions}/>
+            <QuestionLikesRank data={questions}/> */}
+            <div>123</div>
             {/* </Col> */}
         </Container>
     )
