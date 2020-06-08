87d4cd1f773597534a5805769e2fda9489853cae
Ivan Cheung <ivan.cheungckn@gmail.com>
2020-06-08 15:41:50 +0800

    [frontend]del react-vis

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,6 @@ import YoutubeCallBack from './component/YoutubeCallBack';
 import ReportOverall from './component/ReportOverall';
 import {ReportPast} from './component/ReportPast'
 import Navbar from './component/Navbar';
-import { ViewsChartZoomable } from './component/ViewsChartZoomable';
 // import FacebookLogin from './component/FacebookLogin';
 // import FacebookLogin from './component/FacebookLogin';
 
@@ -65,9 +64,6 @@ function App() {
           </Route>
           <Route path="/testing/:lastXMeetings" exact>
           {(isAuthenticated != null && <ReportOverall />)}
-          </Route>
-          <Route path="/zoom" exact>
-          {(isAuthenticated != null && <ViewsChartZoomable />)}
           </Route>
         </Switch>
 
--- frontend/src/component/Home.tsx
+++ frontend/src/component/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Container, Col, Row } from 'react-bootstrap'
 import './Home.scss'
 // const form = useFormState()
 export default function Home() {
@@ -7,15 +6,15 @@ export default function Home() {
             <div>
                 <div id='Home' className="background">
                     <div className='d-flex'>
-                        <Col md={5} className='headings'>
+                        <div className='headings'>
                             <h2>Grab comments from your Facebook & YouTube live streams at a glance.</h2>
                             <h4>Hands UP</h4>
                             <h6>Join NOW!</h6>
                             <textarea placeholder="Enter Meeting code"></textarea>
-                        </Col>
-                        <Col md={7}>
+                        </div>
+                        <div>
 
-                        </Col>
+                        </div>
                     </div>
                 </div>
             </div>
