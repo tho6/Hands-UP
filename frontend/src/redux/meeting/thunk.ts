35577c4f663d6692f324d1f1ecc4f517dedc361c
Tavin Ho <chingching6@gmail.com>
2020-06-13 21:04:11 +0800

    Update Meeting css, Meeting live components

--- frontend/src/redux/meeting/thunk.ts
+++ frontend/src/redux/meeting/thunk.ts
@@ -14,9 +14,9 @@ export function fetchMeeting(meetingId: number) {
         })
         const result = await res.json();
         console.log(result.message);
-        if (!result.message.message) {
-            return      
-        }
+        // if (!result.message.message) {
+        //     return      
+        // }
         dispatch(loadMeetings(result.message))
         return
     }
