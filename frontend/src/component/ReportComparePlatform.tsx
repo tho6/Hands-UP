7a1dfee0559c3c4183329b08cafa314120b07125
Ivan Cheung <ivan.cheungckn@gmail.com>
2020-08-29 18:21:40 +0800

    [frontend] fix compareTableCompoenet

--- frontend/src/component/ReportComparePlatform.tsx
+++ frontend/src/component/ReportComparePlatform.tsx
@@ -29,7 +29,7 @@ export const ReportComparePlatform:React.FC<{currentData:{'youtube':number, 'fac
                         {
                             (currentData['youtube'] && pastData['youtube']) && currentData['youtube'] - pastData['youtube'] > 0? 
                             <><i className="fas fa-arrow-up increase"></i><span className='increase'>+{currentData['youtube'] - pastData['youtube']}</span><span className='increase-percent'>({Math.round((currentData['youtube']! - pastData['youtube']!)/pastData['youtube']!*100)}%)</span></>
-                            :<><i className="fas fa-arrow-down decrease"></i><span className='decrease'>{currentData['youtube']! - pastData['youtube']!}</span><span className='decrease-percent'>({Math.round((currentData['youtube']! - pastData['youtube']!)/pastData['youtube']!*100)}%)</span></>
+                            :<><i className="fas fa-arrow-down decrease"></i><span className='decrease'>{isNaN(currentData['youtube']! - pastData['youtube']!)?"0":currentData['youtube']! - pastData['youtube']!}</span><span className='decrease-percent'>{(isNaN(Math.round((currentData['youtube']! - pastData['youtube']!)/pastData['youtube']!*100))?"0%":Math.round((currentData['youtube']! - pastData['youtube']!)/pastData['youtube']!*100) + "%")}</span></>
                             }
                     </div>
                 </div>
@@ -42,12 +42,11 @@ export const ReportComparePlatform:React.FC<{currentData:{'youtube':number, 'fac
                     </div>
                     <div className="report-compare-platform-compare">
                         
-                        {pastData['facebook'] ?
+                        {
                             (currentData['facebook'] && pastData['facebook']) && currentData['facebook'] - pastData['facebook'] > 0? 
-                            <><i className="fas fa-arrow-up increase"></i><span className='increase'>+{currentData['facebook'] - pastData['facebook']}</span><span className='increase-percent'>{pastData['facebook']!=null?(Math.round((currentData['facebook']! - pastData['facebook']!)/pastData['facebook']!*100)+"%"): "0%"}</span></>
-                            :<><i className="fas fa-arrow-down decrease"></i><span className='decrease'>{currentData['facebook']! - pastData['facebook']!}</span><span className='decrease-percent'>{pastData['facebook']!=null?Math.round((currentData['facebook']! - pastData['facebook']!)/pastData['facebook']!*100)+"%": "0%"}</span></>
-                            : "0%"
-                        }
+                            <><i className="fas fa-arrow-up increase"></i><span className='increase'>+{currentData['facebook'] - pastData['facebook']}</span><span className='increase-percent'>({Math.round((currentData['facebook']! - pastData['facebook']!)/pastData['facebook']!*100)}%)</span></>
+                            :<><i className="fas fa-arrow-down decrease"></i><span className='decrease'>{isNaN(currentData['facebook']! - pastData['facebook']!)?"0":currentData['facebook']! - pastData['facebook']!}</span><span className='decrease-percent'>{(isNaN(Math.round((currentData['facebook']! - pastData['facebook']!)/pastData['facebook']!*100))?"0%":Math.round((currentData['facebook']! - pastData['facebook']!)/pastData['facebook']!*100) + "%")}</span></>
+                            }
                     </div>
                 </div>
                 <div className='report-compare-platform-items'>
@@ -58,10 +57,10 @@ export const ReportComparePlatform:React.FC<{currentData:{'youtube':number, 'fac
                         <span className="currentViews">{currentData['handsup']}</span>
                     </div>
                     <div className="report-compare-platform-compare">
-                        {
+                    {
                             (currentData['handsup'] && pastData['handsup']) && currentData['handsup'] - pastData['handsup'] > 0? 
-                            <><i className="fas fa-arrow-up increase"></i><span className='increase'>+{currentData['handsup'] - pastData['handsup']}</span><span className='increase-percent'>{pastData['handsup']!=null?(Math.round((currentData['handsup']! - pastData['handsup']!)/pastData['handsup']!*100)+"%"): "0%"}</span></>
-                        :<><i className="fas fa-arrow-down decrease"></i><span className='decrease'>{currentData['handsup']! - pastData['handsup']!}</span><span className='decrease-percent'>{pastData['handsup']!=null?Math.round((currentData['handsup']! - pastData['handsup']!)/pastData['handsup']!*100)+"%": "0%"}</span></>
+                            <><i className="fas fa-arrow-up increase"></i><span className='increase'>+{currentData['handsup'] - pastData['handsup']}</span><span className='increase-percent'>({Math.round((currentData['handsup']! - pastData['handsup']!)/pastData['handsup']!*100)}%)</span></>
+                            :<><i className="fas fa-arrow-down decrease"></i><span className='decrease'>{isNaN(currentData['handsup']! - pastData['handsup']!)?"0":currentData['handsup']! - pastData['handsup']!}</span><span className='decrease-percent'>{(isNaN(Math.round((currentData['handsup']! - pastData['handsup']!)/pastData['handsup']!*100))?"0%":Math.round((currentData['handsup']! - pastData['handsup']!)/pastData['handsup']!*100) + "%")}</span></>
                             }
                     </div>
                 </div>
